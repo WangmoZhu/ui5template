d345e32cd068b956128aaca9c86c7c66ff5d102e
WangmoZhu <18702604713@163.com>
2021-01-15 17:15:19 +0800

    add new view

---
+++ webapp/controller/viewName.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "{{controllerToExtend}}"
+  ], function(Controller) {
+    "use strict";
+  
+    return Controller.extend("{{appId}}.controller.{{viewname}}", {
+        
+    });
+  });
